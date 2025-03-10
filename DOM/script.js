f01184a695536694e70a0642c661758a81a19f4f
harshita nagar <harshitanagar52@gmail.com>
2025-03-10 11:43:45 +0530

    digital clock project based on dom

---
+++ DOM/script.js
@@ -0,0 +1,16 @@
+// find the element of html by its id 
+ document.getElementById("h1")
+
+// find the element and add in the html " hello world"
+document.getElementById("hello").innerHTML = "hello world";
+
+// find element by class name 
+document.getElementsByClassName("div")
+
+
+let head= document.getElementsByTagName("h2");
+console.log(head);
+
+function changeText() {
+    document.getElementById("heading").innerHTML = "DOM Manipulated!";
+}
